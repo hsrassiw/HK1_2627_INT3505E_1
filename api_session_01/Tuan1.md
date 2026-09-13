029ca28805f16dd770be47cbd388b4d0e55907be
hsrassiw <minhnghianshd@gmail.com>
2026-09-14 00:21:46 +0700

    Them bai 2 tuan 1

--- api_session_01/Tuan1.md
+++ api_session_01/Tuan1.md
@@ -2,5 +2,17 @@
 
 Chạy server:
 ![Chạy server](bai1_server.png)
+
 Kết quả test:
 ![Gọi API](bai1_curl.png)
+
+## Bài 2
+
+Chạy server:
+![Chạy server](bai2_server.png)
+
+Kết quả test GET /health:
+![Kết quả test health](bai2_health.png)
+
+Kết quả test POST /echo:
+![Kết quả test echo](bai2_echo.png)
--- api_session_01/app.py
+++ api_session_01/app.py
@@ -1,9 +1,25 @@
-#Bai1
-from flask import Flask
+# #Bai1
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route("/")
+# def index():
+#     return {"message": "Hello"}
+# if __name__ == "__main__":
+#     app.run(host="127.0.0.1", port=5000, debug=True)
+
+# Bai2
+from flask import Flask, jsonify, request
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return {"message": "Hello"}
+# GET /health - kiểm tra tình trạng của server
+@app.route("/health", methods = ["GET"])
+def health():
+    return jsonify({"status": "good"}), 200
+
+# POST /echo - phản hồi lại client gửi tới
+@app.route("/echo", methods=["POST"])
+def echo():
+    data = request.get_json(silent=True) or {}
+    return jsonify({"you_sent": data}), 200
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
     
